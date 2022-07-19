dc7c78cf2a6f184fa9e7b783af1cbaa679a44e35
derikbf <derikb@gmail.com>
2022-07-19 20:54:47 -0300

    created model user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,4 +5,3 @@ export default class UsersController {
     return response.created({})
   }
 }
-
--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -4,8 +4,19 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
-  test.only('it should create an user', async () => {
-    const userPayload = {email: 'test', username: 'test', password: 'test'}
-    await supertest(BASE_URL).post('/users').send( userPayload ).expect(201)
+  test.only('it should create an user', async (assert) => {
+    const userPayload = {
+      email: 'test', 
+      username: 'test', 
+      password: 'test', 
+      avatar: 'http://images.com/image/1' 
+    }
+    const { body } = await supertest(BASE_URL).post('/users').send( userPayload ).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.avatar, userPayload.avatar)
+    assert.equal(body.password, userPayload.password)
   })
 })
