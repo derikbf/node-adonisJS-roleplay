bf27b3937cd28d35ba4fdda689a42399058d6e9b
derikbf <derikb@gmail.com>
2022-07-19 23:20:23 -0300

    Created scenarios with invalid data update

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -125,6 +125,47 @@ test.group('User', (group) => {
       await user.refresh()
       assert.isTrue(await Hash.verify(user.password, password))
   })
+  
+  test('it should return 422 when required data is not provided', async(assert) => {
+    const { id } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL).put(`/users/${id}`).send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+    
+  test('it should return 422 when providing an invalid email', async(assert) => {
+    const { id, password, avatar } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL).put(`/users/${id}`).send({
+      password, 
+      avatar, 
+      email: 'test@'
+    }).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+  
+  test('it should return 422 when providing an invalid password', async(assert) => {
+    const { id, email, avatar } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL).put(`/users/${id}`).send({
+      email, 
+      avatar, 
+      password: 'tes'
+    }).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+  test('it should return 422 when providing an invalid avatar', async(assert) => {
+    const { id, email, password } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL).put(`/users/${id}`).send({
+      email, 
+      password, 
+      avatar: 'test'
+    }).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
 
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
