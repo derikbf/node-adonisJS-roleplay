2c139056fc5d90746b0d31fa715dc4a3170235af
derikbf <derikb@gmail.com>
2022-07-19 22:01:38 -0300

    Created scenarios username already exists 409

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -6,9 +6,11 @@ export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
     const userByEmail = await User.findBy('email', userPayload.email)
+    const userByUsername = await User.findBy('username', userPayload.username)
     
-    if (userByEmail)
-      throw new BadRequest('email already in use', 409)
+    if (userByEmail) throw new BadRequest('email already in use', 409)
+
+    if (userByUsername) throw new BadRequest('username already in use', 409)            
     
     const user = await User.create(userPayload)
     return response.created({ user })
