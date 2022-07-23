07f870f7486c94bf8802012c4eeb259f748c065f
derikbf <derikb@gmail.com>
2022-07-22 21:08:44 -0300

    Created scenarios tests Groups

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,4 +1,5 @@
-import { BaseModel, beforeSave, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Group from 'App/Models/Group'
+import { BaseModel, beforeSave, column, hasMany, HasMany, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash';
 import LinkToken from './LinkToken';
@@ -29,6 +30,12 @@ export default class User extends BaseModel {
     foreignKey: 'userId'
   })
   public tokens: HasMany<typeof LinkToken>
+
+  @manyToMany(() => Group, {
+    pivotTable: 'groups_users',
+  })
+  public groups: ManyToMany<typeof Group>
+
 
   @beforeSave()
   public static async hashPassword(user: User) {
