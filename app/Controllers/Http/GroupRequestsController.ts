16ca716bb857e316d2e812ed61440216e81d264d
derikbf <derikb@gmail.com>
2022-07-22 22:08:24 -0300

    Created scenarios tests Request Group exists

---
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -0,0 +1,21 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequest from 'App/Exceptions/BadRequestException';
+import GroupRequest from 'App/Models/GroupRequest'
+
+export default class GroupRequestsController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    const groupId = request.param('groupId') as number
+    const userId  = auth.user!.id
+
+    const existingGroupRequest = await GroupRequest.query()
+      .where('groupId', groupId)
+      .andWhere('userId', userId)
+      .first()
+
+    if (existingGroupRequest) throw new BadRequest('group request already exists', 409)
+
+    const groupRequest = await GroupRequest.create({ groupId, userId }) 
+    await groupRequest.refresh()
+    return response.created({ groupRequest })
+  }
+}
